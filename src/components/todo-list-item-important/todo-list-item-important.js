742eb15a8519e1d0cb86a8f3d49877855672150f
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-03-23 20:23:10 -0700

    Small changes

--- src/components/todo-list-item-important/todo-list-item-important.js
+++ src/components/todo-list-item-important/todo-list-item-important.js
@@ -8,19 +8,24 @@ const TodoListItemImportant = (props) => {
     const todo = props.todo;
     const [newName, setNewName] = useState(todo.todoName);
 
-    let check, done, important;
-
+    let check, done, important, success, warning;
+    console.log('todo.isDone ',todo.isDone);
     if (todo.isDone) {
         check = 'fa-check-circle';
         done = 'done';
+        success = ' success';
+        console.log('success=',success);
     } else {
         check = 'fa-circle';
         done = '';
+        success = 'outline-success';
     }
     if (todo.isImportant) {
         important = 'important';
+        warning = ' warning';
     } else {
         important = '';
+        warning = ' outline-warning';
     }
     const onUndo=(id)=>{
         props.undo(id);
@@ -32,10 +37,10 @@ const TodoListItemImportant = (props) => {
             <div className="card  m-2">
                 <div className='card-body p-0'>
                     <div className='d-flex flex-row justify-content-between align-items-center green'>
-                        <div className='col-sm-0.5 p-2 position-left'>
+                        <div className='col-sm-0.5 p-2 position-left' onClick={() => props.done(todo.todoId)}>
                             <i className={`text-success p-2 position-left far ${check}`}></i>
                         </div>
-
+                        {/*//////////////////_TODO_TASK_/////////////////////////////////////////////////////*/}
                         <div className='col-sm-9 '>
                             {!todo.isEdit && <input type='text'
                                                     className={`form-control border-0 ${done} ${important}`}
@@ -48,18 +53,19 @@ const TodoListItemImportant = (props) => {
                                                    onChange={e => setNewName(e.target.value)}
                                                    autoFocus/>}
                         </div>
+                        {/*//////////////////_DONE<---->UNDO_/////////////////////////////////////////////////////*/}
                         <div className='col-sm-2 d-flex flex-row align-items-center'>
-                            {!todo.isEdit && <button className='col-sm-3 btn btn-success mr-1'
+                            {!todo.isEdit && <button className={`col-sm-3 btn mr-1  ${success} `}
                                                      onClick={() => props.done(todo.todoId)}>
-                                <i className="far fa-check-square"></i>
+                                <i className="far fa-check-square "></i>
                             </button>}
 
                             {todo.isEdit && <button className='col-sm-3 btn btn-outline-info mr-1 '
                                                     onClick={() => onUndo(todo.todoId)}>
                                 <i className="fas fa-undo"></i>
                             </button>}
-
-                            <button className='col-sm-3 btn btn-info mr-1 '
+                            {/*//////////////////_EDIT<---->SAVE_/////////////////////////////////////////////////////*/}
+                            <button className='col-sm-3 btn btn-outline-info mr-1 '
                                     onClick={() => props.edit({
                                         id: todo.todoId,
                                         name: newName
@@ -67,15 +73,16 @@ const TodoListItemImportant = (props) => {
                                 {!todo.isEdit && <i className="fas fa-edit"></i>}
                                 {todo.isEdit && <i className="far fa-save"></i>}
                             </button>
-
-                            {!todo.isEdit && <button className='col-sm-3 btn btn-danger mr-1'
+                            {/*//////////////////_DELETE_/////////////////////////////////////////////////////*/}
+                            {!todo.isEdit && <button className='col-sm-3 btn btn-outline-danger mr-1'
                                                      onClick={() => props.deleteTaskInfo({
                                                          id: todo.todoId,
                                                          name: todo.todoName
                                                      })}>
                                 <i className="far fa-trash-alt"></i>
                             </button>}
-                            {!todo.isEdit && <button className='col-sm-3 btn btn-warning'
+                            {/*//////////////////_IMPORTANT_/////////////////////////////////////////////////////*/}
+                            {!todo.isEdit && <button className={`col-sm-3 btn ${warning}`}
                                                      onClick={() => props.important(todo.todoId)}>
                                 <i className="fas fa-exclamation"></i>
                             </button>}
@@ -87,6 +94,7 @@ const TodoListItemImportant = (props) => {
         </div>
     )
 };
+
 
 const mapStateToProps = (state) => ({
     todoList: state.todoList,
